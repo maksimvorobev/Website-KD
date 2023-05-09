e80ff15b148cd8a63e7701a2e1c6b5d671840d77
maksimvorobev <Max303V@mail.ru>
2023-05-09 17:30:20 +0700

    add layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tabs__content-item");
+
+tabItem.forEach(function (element) {
+  element.addEventListener("click", open);
+});
+
+function open(evt) {
+  const tabTarger = evt.currentTarget;
+  const button = tabTarger.dataset.button;
+
+  tabItem.forEach(function (item) {
+    item.classList.remove("tabs__btn-item_active");
+  });
+  tabTarger.classList.add("tabs__btn-item_active");
+
+  tabContent.forEach(function (item) {
+    item.classList.remove("tabs__content-item_active");
+  });
+  document
+    .querySelector(`#${button}`)
+    .classList.add("tabs__content-item_active");
+}
+
 const scrollController = {
   scrollPosition: 0,
   disabledScroll() {
